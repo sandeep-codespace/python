4ce4810e76fb794c4dd21f266dd994a2c3f51fd6
sandeep-codespace <kumarsandeep1810@gmail.com>
2023-09-19 21:19:19 +0530

    formating  updated

--- day1/taskday1.py
+++ day1/taskday1.py
@@ -1,5 +1,7 @@
 # area of cirle
 # calculated as  πr2
+
+print("\nCalculate area of cirle");
 pie = 3.14;
 radius = input("Please enter circle radius (cm): ");
 radiusValue = int(radius);
@@ -10,8 +12,10 @@ print("Area of circle for "+ radius +"(cm) radius is: ", circleArea);
 
 # simple interest
 # calculated as  SI= (P × R × T)/100
+print("\n\nCalculate simple interest");
+
 principal = input("Please enter Principal amount: ");
-rate = input("Please enter Rate of Interest in %: ");
+rate = input("Please enter Rate of Interest (%): ");
 time = input("Please enter Time (years): ");
 simpleInterest = (int(principal) * int(rate) * int(time))/100;
 
@@ -21,7 +25,10 @@ print("simple interest for P = "+ principal +",R =" +rate+ ", T = "+time+" is: "
 
 # swap two number
 # with three variables
+print("\n\nSwap two numbers using 3 variables:");
 firstValue,secondValue = 8,21;
+
+print("\n\n before swap:\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
@@ -29,13 +36,15 @@ swapVaribale = secondValue;
 secondValue = firstValue;
 firstValue = swapVaribale;
 
-print("After swap");
+print("\n\nAfter swap\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
 
 # with two variables
+print("\n\nSwap two numbers using 2 variables:");
 firstValue,secondValue = 4,5;
+print("\n\n before swap:\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
@@ -43,14 +52,15 @@ secondValue = firstValue + secondValue;
 firstValue = secondValue - firstValue;
 secondValue = secondValue - firstValue;   
 
-print("After swap");
+print("\n\nAfter swap\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
 
 # swap of three numbers
-
+print("\n\nSwap three numbers:");
 firstValue,secondValue, thirdValue = 4,5,7;
+print("\n\n before swap:\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
@@ -61,7 +71,7 @@ thirdValue = firstValue - (secondValue+thirdValue);
 secondValue = firstValue - (secondValue+thirdValue);
 firstValue  = firstValue - (secondValue+thirdValue);
 
-print("After swap");
+print("\n\nAfter swap\n");
 print("First Value:", firstValue);
 
 print("Second Value", secondValue);
